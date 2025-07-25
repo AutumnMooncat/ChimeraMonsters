80499073fef7ea3c974f0745cfbace69b159e687
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2025-07-25 06:30:21 -0300

    AbstractInternalLogicPower no longer needs name given it wont render

--- src/main/java/ChimeraMonsters/powers/AbstractInternalLogicPower.java
+++ src/main/java/ChimeraMonsters/powers/AbstractInternalLogicPower.java
@@ -5,7 +5,7 @@ import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.InvisiblePower;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 
 public abstract class AbstractInternalLogicPower extends AbstractEasyPower implements InvisiblePower {
-    public AbstractInternalLogicPower(String ID, String name, AbstractCreature owner, int amount) {
-        super(ID, name, NeutralPowertypePatch.NEUTRAL, false, owner, amount);
+    public AbstractInternalLogicPower(String ID, AbstractCreature owner, int amount) {
+        super(ID, ID, NeutralPowertypePatch.NEUTRAL, false, owner, amount);
     }
 }
--- src/main/java/ChimeraMonsters/powers/DoppelPower.java
+++ src/main/java/ChimeraMonsters/powers/DoppelPower.java
@@ -23,18 +23,10 @@ import java.util.ArrayList;
 
 public class DoppelPower extends AbstractInternalLogicPower implements IntentInterceptingPower {
     public static final String POWER_ID = ChimeraMonstersMod.makeID(DoppelPower.class.getSimpleName());
-    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
-    public static final String NAME = powerStrings.NAME;
-    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
     public DoppelPower(AbstractCreature owner, int amount) {
-        super(POWER_ID, NAME, owner, amount);
+        super(POWER_ID, owner, amount);
         priority = -5;
-    }
-
-    @Override
-    public void updateDescription() {
-        description = DESCRIPTIONS[0];
     }
 
     @Override
