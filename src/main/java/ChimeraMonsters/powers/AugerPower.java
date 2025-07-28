8eef4f79a4dfa2c3f71ace26c5006d3fb8b14dea
Mindbomber2 <79657039+Mindbomber2@users.noreply.github.com>
2025-07-28 20:39:58 +0200

    fix: AugerMod now works on non-Transient Monsters

--- src/main/java/ChimeraMonsters/powers/AugerPower.java
+++ src/main/java/ChimeraMonsters/powers/AugerPower.java
@@ -3,11 +3,13 @@ package ChimeraMonsters.powers;
 import ChimeraMonsters.ChimeraMonstersMod;
 import ChimeraMonsters.damagemods.AugerDamage;
 import com.evacipated.cardcrawl.mod.stslib.damagemods.AbstractDamageModifier;
+import com.evacipated.cardcrawl.mod.stslib.damagemods.DamageModifierManager;
 import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.DamageModApplyingPower;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import java.util.Collections;
 import java.util.List;
@@ -18,8 +20,11 @@ public class AugerPower extends AbstractEasyPower implements DamageModApplyingPo
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
+    private AbstractDamageModifier augerDamageMod;
+
     public AugerPower(AbstractCreature owner, int amount) {
         super(POWER_ID, NAME, PowerType.BUFF, false, owner, amount);
+        augerDamageMod = new AugerDamage(amount);
     }
 
     @Override
@@ -34,6 +39,17 @@ public class AugerPower extends AbstractEasyPower implements DamageModApplyingPo
 
     @Override
     public List<AbstractDamageModifier> modsToPush(DamageInfo damageInfo, Object o, List<AbstractDamageModifier> list) {
-        return Collections.singletonList(new AugerDamage(amount));
+        return Collections.singletonList(augerDamageMod);
+    }
+
+    @Override
+    public void onInitialApplication() {
+        super.onInitialApplication();
+        if(owner instanceof AbstractMonster){
+            AbstractMonster m = (AbstractMonster) owner;
+            for (DamageInfo damageInfo : m.damage) {
+                DamageModifierManager.BoundDamageInfoFields.boundDamageMods.get(damageInfo).add(augerDamageMod);
+            }
+        }
     }
 }
