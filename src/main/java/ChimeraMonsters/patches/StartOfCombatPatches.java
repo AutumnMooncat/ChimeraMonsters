34201dadc352a57fdd202bae2b5f1c807b142a81
Mindbomber2 <79657039+Mindbomber2@users.noreply.github.com>
2025-08-09 03:36:35 +0200

    WIP: feat: show fight name

--- src/main/java/ChimeraMonsters/patches/StartOfCombatPatches.java
+++ src/main/java/ChimeraMonsters/patches/StartOfCombatPatches.java
@@ -1,8 +1,16 @@
 package ChimeraMonsters.patches;
 
+import ChimeraMonsters.ChimeraMonstersMod;
 import ChimeraMonsters.util.FightModificationManager;
+import basemod.devcommands.fight.Fight;
 import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.monsters.MonsterGroup;
+import com.megacrit.cardcrawl.vfx.combat.BattleStartEffect;
+import javassist.CannotCompileException;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
 
 public class StartOfCombatPatches {
     @SpirePatch2(clz = MonsterGroup.class, method = "init")
@@ -10,6 +18,39 @@ public class StartOfCombatPatches {
         @SpirePrefixPatch()
         public static void plz(MonsterGroup __instance) {
             FightModificationManager.rollFightModifiers(__instance);
+        }
+    }
+
+    public static String getBattleStartText(){
+        return FightModificationManager.fightName.equals("") ? CardCrawlGame.languagePack.getUIString("BattleStartEffect").TEXT[0] : FightModificationManager.fightName;
+    }
+
+    @SpirePatch2(clz = BattleStartEffect.class, method = "render")
+    public static class InsertFightName {
+        @SpireInstrumentPatch
+        public static ExprEditor patch() {
+            return new ExprEditor() {
+                @Override
+                public void edit(MethodCall m) throws CannotCompileException {
+                    ChimeraMonstersMod.logger.info("patch triggered");
+                    if (m.getClassName().equals(FontHelper.class.getName()) && m.getMethodName().equals("renderFontCentered") && m.getLineNumber() ==  223) {
+                        m.replace("$3 = "+StartOfCombatPatches.class.getName()+".getBattleStartText();" +
+                                "$_ = $proceed($$);");
+                    }
+                }
+            };
+        }
+    }
+
+    @SpirePatch2(clz = BattleStartEffect.class, method = "render")
+    public static class MakeFightNameDisappearSlowly {
+        @SpirePostfixPatch()
+        public static void makeSlow(BattleStartEffect __instance) {
+            if(!FightModificationManager.fightName.equals("")){
+                __instance.duration=8.0f;
+                __instance.startingDuration=8.0f;
+                //TODO: FIX
+            }
         }
     }
 }
