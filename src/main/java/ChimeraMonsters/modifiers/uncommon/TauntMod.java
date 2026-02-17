098ab46450540c64e6c9dcb2a7d0e9d853a4ce0f
Mindbomber2 <79657039+Mindbomber2@users.noreply.github.com>
2026-02-18 00:13:50 +0100

    feat: Taunt Modifier

--- src/main/java/ChimeraMonsters/modifiers/uncommon/TauntMod.java
+++ src/main/java/ChimeraMonsters/modifiers/uncommon/TauntMod.java
@@ -3,15 +3,19 @@ package ChimeraMonsters.modifiers.uncommon;
 import ChimeraMonsters.ChimeraMonstersMod;
 import ChimeraMonsters.modifiers.AbstractMonsterModifier;
 import ChimeraMonsters.powers.BackstagePower;
+import ChimeraMonsters.powers.TauntPower;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.monsters.MonsterGroup;
 import com.megacrit.cardcrawl.monsters.beyond.OrbWalker;
 import com.megacrit.cardcrawl.monsters.city.Taskmaster;
 import com.megacrit.cardcrawl.monsters.exordium.Cultist;
+import com.megacrit.cardcrawl.powers.MinionPower;
 
-public class BackstageMod extends AbstractMonsterModifier {
-    public static final String ID = ChimeraMonstersMod.makeID(BackstageMod.class.getSimpleName());
+import java.util.List;
+
+public class TauntMod extends AbstractMonsterModifier {
+    public static final String ID = ChimeraMonstersMod.makeID(TauntMod.class.getSimpleName());
     public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
 
     @Override
@@ -21,19 +25,26 @@ public class BackstageMod extends AbstractMonsterModifier {
 
     @Override
     protected boolean validMonster(AbstractMonster monster, MonsterGroup context) {
-
-        if (context != null && context.monsters != null && context.monsters.size() <= 1) {
-            return false;
-        } else if (monster.id.equals(Taskmaster.ID) || monster.id.equals(Cultist.ID) || monster.id.equals(OrbWalker.ID)) {
-            return false;
-        } else {
+        if (context == null || context.monsters == null) {
             return true;
         }
+        if (context.monsters.size() <= 1 || monster.hasPower(MinionPower.POWER_ID)) {
+            return false;
+        }
+        for (AbstractMonster m : context.monsters) {
+            if (monster.equals(m)) {
+                continue;
+            }
+            if (m.id.equals(Taskmaster.ID) || m.id.equals(Cultist.ID) || m.id.equals(OrbWalker.ID)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public void applyTo(AbstractMonster monster) {
-        applyPowersToCreature(monster, new BackstagePower(monster, 1));
+        applyPowersToCreature(monster, new TauntPower(monster, 1));
     }
 
     @Override
@@ -43,7 +54,7 @@ public class BackstageMod extends AbstractMonsterModifier {
 
     @Override
     public AbstractMonsterModifier makeCopy() {
-        return new BackstageMod();
+        return new TauntMod();
     }
 
     @Override
--- src/main/java/ChimeraMonsters/powers/TauntPower.java
+++ src/main/java/ChimeraMonsters/powers/TauntPower.java
@@ -3,47 +3,45 @@ package ChimeraMonsters.powers;
 import ChimeraMonsters.ChimeraMonstersMod;
 import ChimeraMonsters.powers.interfaces.MonsterPreventPlayingCardsPower;
 import ChimeraMonsters.powers.interfaces.RenderModifierPower;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.SpeechBubble;
+import com.megacrit.cardcrawl.vfx.SpeechTextEffect;
 
-public class BackstagePower extends AbstractInternalLogicPower implements RenderModifierPower, MonsterPreventPlayingCardsPower {
-    public static final String POWER_ID = ChimeraMonstersMod.makeID(BackstagePower.class.getSimpleName());
+public class TauntPower extends AbstractInternalLogicPower implements RenderModifierPower, MonsterPreventPlayingCardsPower {
+    public static final String POWER_ID = ChimeraMonstersMod.makeID(TauntPower.class.getSimpleName());
     private static final UIStrings cantUseText = CardCrawlGame.languagePack.getUIString(ChimeraMonstersMod.makeID("CantUseText"));
+    private static final UIStrings speechText = CardCrawlGame.languagePack.getUIString(ChimeraMonstersMod.makeID("SpeechText"));
+    private final float speechCooldown = 4f;
+    private float remainingSpeechCooldown = speechCooldown;
 
-    public BackstagePower(AbstractCreature owner, int amount) {
+    public TauntPower(AbstractCreature owner, int amount) {
         super(POWER_ID, owner, amount);
+
     }
 
     @Override
     public void onRender(SpriteBatch sb, TextureRegion tex) {
         Color origColor = sb.getColor();
-        //sb.setColor(Color.WHITE);
-        if (!isLast()) {
-            sb.setColor(new Color(0.75f, 0.7f, 0.75f, 1f));
-            render(sb, tex, 0, 150, 0.6f, 10);
-        } else {
-            sb.setColor(Color.WHITE);
-            render(sb, tex);
+        sb.setColor(new Color(1f, 0.7f, 0.75f, 1f));
+        render(sb, tex, 0, 0, 1.2f, 0);
+        this.remainingSpeechCooldown -= Gdx.graphics.getDeltaTime();
+        if (this.remainingSpeechCooldown < 0.0F) {
+            AbstractDungeon.effectList.add(new SpeechBubble(owner.hb.cX + (-50.0F * Settings.scale), owner.hb.cY + (70.0F * Settings.scale), 2.0F, speechText.TEXT[0], false));
+            remainingSpeechCooldown = speechCooldown;
         }
-
         sb.setColor(origColor);
     }
 
-    public boolean isLast() {
-        for (AbstractMonster mon : AbstractDungeon.getMonsters().monsters) {
-            if (!mon.isDeadOrEscaped() && !mon.equals(owner)) {
-                return false;
-            }
-        }
-        return true;
-    }
 
     @Override
     public boolean preventPlaying(AbstractCard card) {
@@ -52,15 +50,11 @@ public class BackstagePower extends AbstractInternalLogicPower implements Render
 
     @Override
     public boolean preventUsing(AbstractCard card, AbstractMonster m) {
-        if (m == null || !m.hasPower(BackstagePower.POWER_ID)) {
-            card.cantUseMessage = null;
-            return false;
-        }
-        if (isLast()) {
+        if (m == null || m.hasPower(TauntPower.POWER_ID)) {
             card.cantUseMessage = null;
             return false;
         } else {
-            card.cantUseMessage = cantUseText.TEXT[0];
+            card.cantUseMessage = cantUseText.TEXT[1];
             return true;
         }
 
